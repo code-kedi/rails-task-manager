84e03874ef0614f121f00153a8108f456fd50fe0
Sonja Peschke <sonjape91@gmail.com>
2022-08-23 16:29:34 +0200

    Rails Task Manager - current status

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,49 @@
 class ApplicationController < ActionController::Base
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  def index
+    # model (logic)
+    @tasks = Task.all
+    # render the view
+  end
+
+  def show
+    id = params[:id]
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    # We need strong params to avoid maliscious users adding suspicious data (it's required)
+    # We create an instance of a restaurant
+    @task = Task.new(tasks_params)
+    # We save it to our database
+    @task.save
+    # We redirect to the show page of the new task
+    redirect_to task_path(@task.id)
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(tasks_params)
+    redirect_to task_path(@task.id)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def tasks_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  # get "tasks", to: "tasks#index", as: :tasks # => tasks_path
+  # Send a post request on submission to create record in database
+  # post "tasks", to: "tasks#create"
+  # User needs form to be able to input data about new task
+  # get "tasks/new", to: "tasks#new", as: :new_task
+  # get "tasks/:id", to: "tasks#show", as: :task # => task_path(2)
+  # patch "tasks/:id", to: "tasks#update"
+  # delete "tasks/:id", to: "tasks#destroy"
+  # get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  Rails.application.routes.draw do
+    resources :tasks
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
